1cdf6cc384efbbfbede447a6007e35332015d487
xxq <xxq_work@163.com>
2020-07-21 23:00:20 +0800

    [print receipt with given format]

--- main.js
+++ main.js
@@ -1,14 +1,107 @@
 function printReceipt(barcodes) {
-    console.log(`
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************`)
+    let productsInfo = getProductsInfoByBarcodes(barcodes);
+    let receipt = createReceipt(productsInfo);
+    console.log(receipt);
 }
 
+function getProductsInfoByBarcodes(barcodes) {
+    let allBarcodes = getAllBarcodes();
+    let counts = countBarcodes(barcodes);
+    return siftProductsInfo(allBarcodes, counts);
+}
+
+function getAllBarcodes() {
+    return [{
+            barcode: 'ITEM000000',
+            name: 'Coca-Cola',
+            price: 3
+        },
+        {
+            barcode: 'ITEM000001',
+            name: 'Sprite',
+            price: 3
+        },
+        {
+            barcode: 'ITEM000002',
+            name: 'Apple',
+            price: 5
+        },
+        {
+            barcode: 'ITEM000003',
+            name: 'Litchi',
+            price: 15
+        },
+        {
+            barcode: 'ITEM000004',
+            name: 'Battery',
+            price: 2
+        },
+        {
+            barcode: 'ITEM000005',
+            name: 'Instant Noodles',
+            price: 4
+        }
+    ]
+}
+
+function countBarcodes(barcodes) {
+    let counts = {};
+    barcodes.forEach(barcode => {
+        if (counts.hasOwnProperty(barcode)) {
+            counts[barcode]++;
+        } else {
+            counts[barcode] = 1;
+        }
+    });
+    return counts;
+}
+
+function siftProductsInfo(allBarcodes, countBarcodes) {
+    let productsInfo = {};
+    const barcodes = Object.keys(countBarcodes);
+    allBarcodes.forEach(item => {
+        if (barcodes.indexOf(item.barcode) !== -1) {
+            productsInfo[item.barcode] = { 'name': item.name, 'price': item.price, 'count': countBarcodes[item.barcode] };
+        }
+    })
+    return productsInfo;
+}
+
+function createReceipt(productsInfo) {
+    let subtotalAmounts = caculateSubtotal(productsInfo);
+    let totalAmount = caculateTotal(subtotalAmounts);
+    return pieceReceipt(subtotalAmounts, totalAmount, productsInfo);
+}
+
+function caculateSubtotal(productsInfo) {
+    let subtotalAmounts = {};
+    for (let key in productsInfo) {
+        subtotalAmounts[key] = productsInfo[key].count * productsInfo[key].price;
+    };
+    return subtotalAmounts;
+}
+
+function caculateTotal(subtotalAmounts) {
+    let total = 0;
+    for (let key in subtotalAmounts) {
+        total += subtotalAmounts[key];
+    };
+    return total;
+}
+
+function pieceReceipt(subtotalAmounts, totalAmount, productsInfo) {
+    let receipt = '\n***<store earning no money>Receipt ***\n';
+    for (let key in productsInfo) {
+        receipt += 'Name: ' + productsInfo[key].name +
+            ', Quantity: ' + productsInfo[key].count +
+            ', Unit price: ' + productsInfo[key].price +
+            ' (yuan), Subtotal: ' + subtotalAmounts[key] + ' (yuan)\n';
+    }
+    receipt += '----------------------\n';
+    receipt += 'Total: ' + totalAmount + ' (yuan)\n';
+    receipt += '**********************';
+    return receipt;
+}
 
 module.exports = {
     printReceipt
---
+++ pdca.md
@@ -0,0 +1,59 @@
+###### printReceipt
+- P: 2 min
+- D: 1 min 
+- C: There is a clear structure 'cause drew a context map.
+- A: It is a goog habbit.
+
+###### getProductsInfoByBarcodes
+- P: 3 min
+- D: 1 min
+- C: Just call functions, have no business logic.
+- A: None
+
+###### getAllBarcodes
+- P: 1 min
+- D: 1 min
+- C: Just paste exist data into function.
+- A: None.
+
+###### countBarcodes
+- P: 5 min
+- D: 6 min
+- C: Fotgot the usage of forEach and how to judge a key is contained in an object. Took 2 minutes to study them.
+- A: Knowledge will be forgotten if not used. Need regular consolidation. 
+
+###### siftProductsInfo
+- P: 10 min
+- D: 12 min
+- C: Didn't know how to get all key of an object and took 1 minute to study it.  Another minute was for coding.
+- A: Should learn some functions adjective.  
+
+###### createReceipt
+- P: 2 min
+- D: 1 min
+- C: Just call functions, have no business logic.
+- A: None.
+
+###### caculateSubtotal
+- P: 2 min
+- D: 2 min
+- C: Didn't know how to traverse an object and took 1 minute to study it.
+- A: Should learn some functions adjective.  
+
+###### caculateTotal
+- P: 2 min
+- D: 2 min
+- C: Forgot return value.
+- A: Be careful!!!
+
+###### pieceReceipt
+- P: 5 min
+- D: 6 min
+- C: Coding overtime.
+- A: Correctly estimate the encoding time.
+
+###### pass the test
+- P: 1 time
+- D: 2 times
+- C: Ignored a '\n' character in first line of the output.
+- A: Verify the requirements carefully.
